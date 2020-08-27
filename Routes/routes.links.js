88a7957d05194f7645984e9e35514305ea95dae8
MahmoudElwan <mahmoudelwan460@gmail.com>
2020-08-27 18:15:56 +0200

    validation
    validation

--- Routes/routes.links.js
+++ Routes/routes.links.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const database = require("../model/DB");
+const {check , validationResult}= require("express-validator")
 
 //display Product
 router.get("/" , async(req,res,next)=>{
@@ -8,14 +9,29 @@ router.get("/" , async(req,res,next)=>{
 })
 router.get("/admin" , async(req,res,next)=>{
     const productContainer=  await database.find({});
-       res.render("Admin",{productContainer})
+       res.render("Admin",{productContainer , messageErrors:[] ,oldInputs:{name:'',description :'',price :'', total :'', imgURL:''}})
    })
 //add Product
-router.post("/handleForm" , async(req,res,next)=>{
-    console.log(req.file.path);
+router.post("/handleForm" ,
+check('name',"invalid data").matches(/^[A-Z a-z]/) ,
+check('price',"invalid data").matches(/^[0-9]{1,500}$/) ,
+check('total',"invalid data").matches(/^[0-9]{1,500}$/) ,
+check('description',"invalid data").matches(/^[A-Z a-z]/) ,
+async(req,res,next)=>{
+    let errors = validationResult(req);
+    console.log(errors);
+    //console.log(req.file.path);
     const {name ,description ,price , total } = req.body;
-    await database.insertMany({name ,description ,price , total , imgURL:req.file.path});
-    res.redirect("/admin");
+    if(errors.isEmpty())
+    {
+        await database.insertMany({name ,description ,price , total , imgURL:req.file.path});
+        res.redirect("/admin");
+    }
+    else
+    {
+    const productContainer=  await database.find({});
+    res.render("Admin",{productContainer, messageErrors:errors.array(), oldInputs:{name ,description ,price , total , imgURL:req.file.path}})
+    }
 })
 //Delete Product
 router.get("/delete/:id" , async(req,res,next)=>{
@@ -27,15 +43,30 @@ router.get("/delete/:id" , async(req,res,next)=>{
 //Edit Product
 router.get("/edit/:id" , async(req,res,next)=>{
     const productContainer= await database.findById({_id:req.params.id});
-    res.render("edit",{productContainer})
+    res.render("edit",{productContainer ,messageErrors:[] ,oldInputs:{name:'',description :'',price :'', total :'', imgURL:''}})
 })
 
-router.post("/editform/:id" , async(req,res,next)=>{
+router.post("/editform/:id" ,
+check('name',"invalid data").matches(/^[A-Z a-z]/) ,
+check('price',"invalid data").matches(/^[0-9]{1,500}$/) ,
+check('total',"invalid data").matches(/^[0-9]{1,500}$/) ,
+check('description',"invalid data").matches(/^[A-Z a-z]/) ,
+ async(req,res,next)=>{
+     const errors= validationResult(req);
     const {name ,description ,price , total } = req.body;
-  await database.findByIdAndUpdate({_id:req.params.id},
-        {name ,description ,price , total , imgURL:req.file.path}
-    );
-    res.redirect("/admin");
+    if(errors.isEmpty())
+    {
+        await database.findByIdAndUpdate({_id:req.params.id},
+            {name ,description ,price , total , imgURL:req.file.path}
+        );
+        res.redirect("/admin");
+    }
+    else
+    {
+        const productContainer=  await database.find({});
+        res.render("edit",{productContainer, messageErrors:errors.array(), oldInputs:{name ,description ,price , total , imgURL:req.file.path}})   
+    }
+  
 })
 //showmore
 router.get("/showmore/:id", async(req,res,next)=>{
